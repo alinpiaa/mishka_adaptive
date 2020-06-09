1a4a517acaab0e7bc0623ca4322ef95bbf8bddfd
Alina Piletskaya <alinpia.work@gmail.com>
2020-06-09 20:52:35 +0300

    added map; added contacts mobile styling

---
+++ src/js/map.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var mapJS = document.querySelector('#map');
+    var mapStatic = document.querySelector('.contacts__map_no-js');
+    
+    var hiddenClass = 'contacts__map_hidden';
+  
+    mapJS.classList.remove(hiddenClass);
+    mapStatic.classList.add(hiddenClass);
+  
+    if (mapJS) {
+        ymaps.ready(init); // eslint-disable-line no-undef
+    }
+  
+    var map;
+  
+    function init() {
+        map = new ymaps.Map('map', { // eslint-disable-line no-undef
+            center: [59.9387165, 30.32318739999995],
+            zoom: 18,
+            controls: []
+        });
+        
+        var placemark = new ymaps.Placemark([59.9387165, 30.32304739999995], {}, { // eslint-disable-line no-undef
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icon-map-pin.svg',
+            iconImageSize: [67, 100]
+        });
+
+        map.geoObjects.add(placemark);
+    }
+});
+  
