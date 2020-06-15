5753d2a0aec9b7f90eda7de980a003afeff99109
Alina Piletskaya <alinpia.work@gmail.com>
2020-06-15 11:24:14 +0300

    added script for menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,9 @@
-function menu() { // eslint-disable-line
-    console.log('some test menu code');
-}
+var menu = document.querySelector('.menu');
+var menuButton = document.querySelector('.menu__btn');
+
+menu.classList.remove('menu_no-js');
+document.body.classList.remove('no-js');
+
+menuButton.addEventListener('click', function() {
+    menu.classList.toggle('menu_opened');
+});
